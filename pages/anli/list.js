25a7fe8f174e756d54ac620ec38bc8736aca4548
hardif <1477821350@qq.com>
2019-12-04 10:26:01 +0800

    案例列表调整1.0

--- pages/anli/list.js
+++ pages/anli/list.js
@@ -1,88 +1,90 @@
-var app=getApp();//获取app
+var app = getApp(); //获取app
 
 var http_url = app.globalData.http_api + "&function=dr_my_list&param=list action=module module=anli page=1 pagesize=10";
 
 Page({
 
-    /**
-     * 页面的初始数据
-     */
-    data: {
+  /**
+   * 页面的初始数据
+   */
+  data: {
 
-        listData:[],
-        hidden: true,
-        page: 1,
-        hasMore:"false"
-    },
+    listData: [],
+    hidden: true,
+    page: 1,
+    hasMore: "false"
+  },
 
-    onLoad:function(options){
-        app.showModel();
-        var self=this;
-        wx.request({
-            url: http_url,
-            method: 'GET',
-            success: function(res){
-                wx.hideLoading();
-                console.log(res.data);
-                if (res.data.code == 1) {
-                    self.setData({
-                        listData:res.data.return,
-                        page: 1
-                    });
-                } else {
-                    console.log(res.data.msg);
-                    wx.showModal({
-                      showCancel: false,
-                      content: res.data.msg
-                    })
-                }
+  onLoad: function(options) {
+    app.showModel();
+    var self = this;
+    wx.request({
+      url: http_url,
+      method: 'GET',
+      success: function(res) {
+        wx.hideLoading();
+        console.log(res.data);
+        if (res.data.code == 1) {
+          self.setData({
+            listData: res.data.return,
+            page: 1
+          });
+        } else {
+          console.log(res.data.msg);
+          wx.showModal({
+            showCancel: false,
+            content: res.data.msg
+          })
+        }
 
-            }
+      }
 
-        })
-    },
-    onReachBottom:function(){
+    })
+  },
+  onReachBottom: function() {
 
-        app.showModel();
-        this.setData({hidden:false});
-        var self=this;
-        var pageid = self.data.page + 1;
+    app.showModel();
+    this.setData({
+      hidden: false
+    });
+    var self = this;
+    var pageid = self.data.page + 1;
 
-        wx.request({
-          url: http_url + "&page=" + pageid,
-            method: 'GET',
-            success: function(res){
+    wx.request({
+      url: http_url + "&page=" + pageid,
+      method: 'GET',
+      success: function(res) {
+        console.log(res);
+        wx.hideLoading();
+        if (res.data.code == 1) {
+          if (res.data.return.length == 0) {
+            self.setData({
+              hasMore: "true",
+              hidden: false
+            });
+            setTimeout(function() {
+              self.setData({
+                hasMore: "false",
+                hidden: true
+              });
+            }, 900)
+          } else {
+            self.setData({
+              listData: res.data.return,
+              hidden: true,
+              page: pageid
+            });
+          }
+        } else {
+          console.log(res.data.msg);
+          wx.showModal({
+            showCancel: false,
+            content: res.data.msg
+          })
+        }
 
-                wx.hideLoading();
-                if (res.data.code == 1) {
-                  if (res.data.return.length==0){
-                        self.setData({
-                            hasMore:"true",
-                            hidden:false
-                        });
-                        setTimeout(function(){
-                            self.setData({
-                                hasMore:"false",
-                                hidden:true
-                            });
-                        },900)
-                    }else{
-                        self.setData({
-                          listData: res.data.return,
-                            hidden:true,
-                            page:pageid
-                        });
-                    }
-                } else {
-                    console.log(res.data.msg);
-                    wx.showModal({
-                      showCancel: false,
-                      content: res.data.msg
-                    })
-                }
-
-            }
-        })
-    }
+      }
+    })
+  }
 
 })
