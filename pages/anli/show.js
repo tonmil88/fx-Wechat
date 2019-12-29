26d7f4013c7b7edeb75476eadac59bdf6f058f24
hardif <1477821350@qq.com>
2019-12-29 16:03:45 +0800

    改案例列表与画廊页并添加案例详情

--- pages/anli/show.js
+++ pages/anli/show.js
@@ -5,74 +5,82 @@ var app = getApp();
 var http_url = app.globalData.http_api + "&function=dr_my_list2&param=list action=content module=anli id=";
 
 var member_url = app.globalData.member_api;
- 
+
 
 Page({
-  data:{
-      id:'',
-      content:'',
-      supports: 0,
-      upsImg:"../../icons/ups.png",
-      collectImg:"../../icons/collect.png",
+  data: {
+    id: '',
+    content: '',
+    supports: 0,
+    upsImg: "../../icons/ups.png",
+    collectImg: "../../icons/collect.png",
 
-      anlitu: [],
-      index:"",
-      indicatorDots: true,
-      vertical: false,
-      autoplay: false,
-      interval: 2000,
-      duration: 500,
-      current:0 //轮播图下标
+    anlitu: [],
+    index: "",
+    indicatorDots: true,
+    vertical: false,
+    autoplay: false,
+    interval: 2000,
+    duration: 500,
+    current: '' ,//轮播图下标
+    idx:''
   },
-  
-  onLoad:function(options){
-      console.log(options);
-      app.showModel();
-      var self=this;
-      wx.request({
-        url: http_url + options.id,
-        header: {
-          'content-type': 'application/json'
-        },
-        dataType: 'json',
-        method: 'GET',
-        success: function (res) {
 
-         
-          if (res.data.code == 1) {
-
-            for (var i in res.data.return) {//不使用过滤
-              var c = res.data.return[i];
-              console.log(c.images);
-            }
-
-            // 格式化案例内容
-            //var article = c.content;
-            var article = '';//案例没内容
-
-            WxParse.wxParse('data', 'html', article, self);
-
-            self.setData({
-              content: c,
-              anlitu:c.images,
-              id: options.id
-            })
-            wx.hideToast();
-          } else {
-            wx.showModal({
-              showCancel: false,
-              content: res.data.msg
-            })
-          }
-
-
-
-        }
-      })
-  },
-  changeCurrent: function (e) {//监听并获取轮播图下标
+  onLoad: function(options) {
+    app.showModel();
+    var anlitu = JSON.parse(options.anlitu)
+    var index = options.index
+    console.log(options);
     this.setData({
-      current: e.detail.current   //获取当前轮播图片的下标
+      anlitu: anlitu,
+      idx: index
+      // current: index
+    })
+    // var self=this;
+    // wx.request({
+    //   url: http_url + options.id,
+    //   header: {
+    //     'content-type': 'application/json'
+    //   },
+    //   dataType: 'json',
+    //   method: 'GET',
+    //   success: function (res) {
+
+
+    //     if (res.data.code == 1) {
+
+    //       for (var i in res.data.return) {//不使用过滤
+    //         var c = res.data.return[i];
+    //         console.log(c.images);
+    //       }
+
+    //       // 格式化案例内容
+    //       //var article = c.content;
+    //       var article = '';//案例没内容
+
+    //       WxParse.wxParse('data', 'html', article, self);
+
+    //       self.setData({
+    //         content: c,
+    //         anlitu:c.images,
+    //         id: options.id
+    //       })
+    //       wx.hideToast();
+    //     } else {
+    //       wx.showModal({
+    //         showCancel: false,
+    //         content: res.data.msg
+    //       })
+    //     }
+
+
+
+    //   }
+    // })
+  },
+  changeCurrent: function(e) { //监听并获取轮播图下标
+    this.setData({
+      current: e.detail.current //获取当前轮播图片的下标
     })
   },
   //  getCommentList:function(){//评论跳转
@@ -83,41 +91,41 @@ Page({
   //  },
 
 
-   collect: function (){//收藏
-     var self =this;
-     wx.request({
-       url: member_url + '&s=anli&c=api&m=favorite&id=' + self.data.id,
+  collect: function() { //收藏
+    var self = this;
+    wx.request({
+      url: member_url + '&s=anli&c=api&m=favorite&id=' + self.data.id,
 
-       data: {
-         is_ajax: 1,
-       },
-       header: {
-         'content-type': 'application/x-www-form-urlencoded'
-       },
-       dataType: 'text',
-       method: 'POST',
-       success: function (sc) {
-         if (sc == 1) {
-           wx.showModal({
-             showCancel: false,
-             content: "没有登录，不能收藏"
-           })
-         } else if (sc == 2) {
-           wx.showModal({
-             showCancel: false,
-             content: "文档不存在，无法收藏"
-           })
-         } else {
-           wx.showToast({
-             icon: 'success',
-             title: "收藏成功",
-             duration: 2000
-           });
-         }
-       }
-     });
+      data: {
+        is_ajax: 1,
+      },
+      header: {
+        'content-type': 'application/x-www-form-urlencoded'
+      },
+      dataType: 'text',
+      method: 'POST',
+      success: function(sc) {
+        if (sc == 1) {
+          wx.showModal({
+            showCancel: false,
+            content: "没有登录，不能收藏"
+          })
+        } else if (sc == 2) {
+          wx.showModal({
+            showCancel: false,
+            content: "文档不存在，无法收藏"
+          })
+        } else {
+          wx.showToast({
+            icon: 'success',
+            title: "收藏成功",
+            duration: 2000
+          });
+        }
+      }
+    });
 
-   }
+  }
 
 
 })
