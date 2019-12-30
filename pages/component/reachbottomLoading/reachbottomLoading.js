c4b7e502f278bbb3830f957c2a1ebf503c280293
tonmil88 <1597943743@qq.com>
2019-12-30 10:18:56 +0800

    全部整理

--- pages/component/reachbottomLoading/reachbottomLoading.js
+++ pages/component/reachbottomLoading/reachbottomLoading.js
@@ -1,69 +1,3 @@
-<<<<<<< HEAD
-// pages/component/reachbottomLoading/reachbottomLoading.js
-Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-=======
 Component({
   properties: {
     // 这里定义了innerText属性，属性值可以在组件使用时指定
@@ -85,6 +19,5 @@ Component({
         hasMore:this.data.hasMore
       })
      }
->>>>>>> 26d7f4013c7b7edeb75476eadac59bdf6f058f24
   }
 })
