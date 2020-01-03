c947fcf0ac4571c152401fc3bde1421240be91fe
hardif <1477821350@qq.com>
2020-01-03 08:49:37 +0800

    修改冲突

--- pages/component/reachbottomLoading/reachbottomLoading.js
+++ pages/component/reachbottomLoading/reachbottomLoading.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-// pages/component/reachbottomLoading/reachbottomLoading.js
 Page({
 
   /**
@@ -12,79 +10,79 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
-=======
-Component({
-  properties: {
-    // 这里定义了innerText属性，属性值可以在组件使用时指定
-    innerText: {
-      type: String,
-      value: 'default value',
-    }
-  },
-  data: {
-    // 这里是一些组件内部数据
-    hidden: true,
-    hasMore: "false",
-  },
-  methods: {
-    // 这里是一个自定义方法
-    showLoad: function () {
-      this.triggrEvent('showLoad',{
-        hidden:this.data.hidden,
-        hasMore:this.data.hasMore
-      })
-     }
->>>>>>> 26d7f4013c7b7edeb75476eadac59bdf6f058f24
+    Component({
+      properties: {
+        // 这里定义了innerText属性，属性值可以在组件使用时指定
+        innerText: {
+          type: String,
+          value: 'default value',
+        }
+      },
+      data: {
+        // 这里是一些组件内部数据
+        hidden: true,
+        hasMore: "false",
+      },
+      methods: {
+        // 这里是一个自定义方法
+        showLoad: function() {
+          this.triggrEvent('showLoad', {
+            hidden: this.data.hidden,
+            hasMore: this.data.hasMore
+          })
+        }
+      }
+    })
   }
 })
