93d25404f56ee4abfe95546a59d0948b0b9733ef
hardif <1477821350@qq.com>
2020-01-15 11:14:54 +0800

    首页静态页面完成，原来的首页变成index2

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,111 +1,143 @@
-var app = getApp();
-var http_url = app.globalData.http_api + "&function=dr_my_list&param=list action=module module=news num=2";
-
+// pages/component/index/index.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    sdData: [],
-    fabuData: [],
-    banners: [
-      'http://www.e-homes.com.cn/statics/gw/pc/pic/tuiguang/taocan2/banner.jpg',
-      'http://oss.e-homes.com.cn/201911/030880687f.jpg',
+    background: ['demo-text-1', 'demo-text-2', 'demo-text-3'],
+    indicatorDots: true,
+    autoplay: false,
+    interval: 2000,
+    duration: 500,
+    inputVal: "",
+    designerFlag: true,//显示设计师类型
+    fenggeFlag: true,//显示擅长风格
+    designerData: [],
+    city: ['佛山', '广州', '东莞', '肇庆'],
+    array: ['美国', '中国', '巴西', '日本'],
+    objectHx: [
+      { value: "", name: "不限" },
+      { value: "xdjy", name: "现代简约" },
+      { value: "ms", name: "美式" },
+      { value: "xzs", name: "新中式" },
+      { value: "bo", name: "北欧" },
+      { value: "gy", name: "工业" },
+      { value: "os", name: "欧式" },
+      { value: "qs", name: "轻奢" },
+      { value: "ty", name: "田园" },
+      { value: "fs", name: "法式" },
     ],
-    icons: [
-      {
-        icon: '../../images/i1.jpg',
-        name: '账号资料',
-        url: '../member/my',
-      },
-      {
-        icon: '../../images/i2.jpg',
-        name: '新闻列表',
-        url: '../news/list',
-      },
-      {
-        icon: '../../images/i4.jpg',
-        name: '我的收藏',
-        url: '../member/favorite',
-      },
-      {
-        icon: '../../images/i3.jpg',
-        name: '我的评论',
-        url: '../member/comment',
+    city_index:0
+  },
+
+  showPicker_hx: function () {
+    console.log(2332)
+    this.setData({
+      fenggeFlag: false
+    })
+  },
+  bindPickerChange_city: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      city_index: e.detail.value
+    })
+  },
+
+  bindPickerChange_hx: function (e) {
+    var judge = this.data.objectHx[e.detail.value].value
+    var shuaixuan = " LIKE_sjsfg=%" + this.data.objectHx[e.detail.value].value + "%"
+    this.setData({
+      index_fg: e.detail.value,
+      shuaixuan: shuaixuan
+    })
+    console.log(this.data.shuaixuan)
+    wx.request({
+      url: http_url + this.data.shuaixuan,
+      method: 'GET',
+      success: (res) => {
+        wx.hideLoading();
+        console.log(res.data.return);
+        if (res.data.code == 1) {
+          this.setData({
+            designerData: res.data.return,
+            page: 1
+          })
+        } else {
+          console.log(res.data.msg);
+          wx.showModal({
+            showCancel: false,
+            content: res.data.msg
+          })
+        }
       }
-    ],
+    })
+  },
+  inputTyping: function (e) {
+    this.setData({
+      inputVal: e.detail.value
+    });
+  },
+  clearInput: function () {
+    console.log(32)
+    this.setData({
+      inputVal: ""
+    });
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    app.showModel();
-    var self = this;
-    wx.request({
-      url: http_url,
-      method: 'GET',
-      success: function (res) {
-        wx.hideLoading();
-        if (res.data.code == 1) {
-          self.setData({
-            fabuData: res.data.return,
-          });
-        }
 
-      }
-
-    });
-    
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
